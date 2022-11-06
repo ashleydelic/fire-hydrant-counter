2bdd1786390f6eaf6d302eb3c4d682155de6a3f0
ashleydelic <105756457+ashleydelic@users.noreply.github.com>
2022-11-06 17:13:00 -0500

    Add files via upload
    increment button is now reactive

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.getElementById("count-el").innerText = 5
+
+let countEl = document.getElementById("count-el");
+console.log("count-el");
+
+let count = 0;
+
+function increment() {
+    count = count + 1;
+    countEl.innerText = count;
+    console.log(count);
+
+}
