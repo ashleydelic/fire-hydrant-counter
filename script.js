c2319093b9dbabe53cfabdba63b8d247d9ec7224
ashleydelic <105756457+ashleydelic@users.noreply.github.com>
2022-11-07 20:49:41 -0500

    Add files via upload
    final javascript clean code

--- script.js
+++ script.js
@@ -9,18 +9,17 @@ function increment() {
     count += 1;
     countEl.innerText = count;
     console.log(count);
-
 }
 
 function save() {
     let savedCount = count + " - ";
     saveEL.textContent += savedCount;
     countEl.textContent = 0;
+    count = 0;
 }
 
 function remove() {
     count -= 1;
     countEl.innerText = count;
-    console.log(count)
-
+    console.log(count);
 }
